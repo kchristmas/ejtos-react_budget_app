235afb9731f7cf28aacdfae218f2f75142f55047
kchristmas <kirstinchristmas@gmail.com>
2024-06-04 15:59:49 -0400

    budget problems

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,17 +1,36 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget , dispatch } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+
     const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
-    }
+        const updatedBudget = parseInt(event.target.value);
+
+        if (updatedBudget >= 20000){
+            alert("The value cannot exceed £20,000");
+                setNewBudget("");
+                return;
+        } else if (updatedBudget< totalExpenses){
+           alert("You don't have the budget for that");
+            return;
+        } else {
+        setNewBudget(updatedBudget);
+        dispatch({
+            type:"SET_BUDGET",
+            payload: updatedBudget,
+        })
+        };    
+        useEffect(() =>{
+            setNewBudget(budget);
+        }, [budget]);
     return (
 <div className='alert alert-secondary'>
 <span>Budget: £{budget}</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+<input type="number" step="10" value={newBudget} onChange={handleBudgetChange()}></input>
 </div>
     );
 };
+}
 export default Budget;
