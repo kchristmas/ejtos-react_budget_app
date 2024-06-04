e93b5cb2e414b05ee4716840c4aab3a64731c04b
kchristmas <kirstinchristmas@gmail.com>
2024-06-04 14:29:37 -0400

    changes made in lab enviroment practice

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from './components/Budget';
 
 // Add code to import the other components here under
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
+import ExpenseItem from './components/ExpenseItem';
+import AllocationForm from './components/AllocationForm';
 
 
 import { AppProvider } from './context/AppContext';
@@ -16,27 +21,45 @@ const App = () => {
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
                         {
-                            /* Add Budget component here */
+                            // Budget component
+                            <div className='col-sm'>
+                                <Budget />
+                            </div>
                         }        
 
                         {
-                            /* Add Remaining component here*/
+                            // Remaing component
+                            <div className='col-sm'>
+                                <Remaining/>
+                            </div>
                         }        
 
                         {
-                            /* Add ExpenseTotal component here */
+                            // Expense total component
+                            <div className='col-sm'>
+                                <ExpenseTotal/>
+                            </div>
                         }        
                        
                         {
-                            /* Add ExpenseList component here */
+                            // Expense list component
+                            <div className='col-sm'>
+                                <ExpenseList/>
+                            </div>
                         }         
 
                         {
-                            /* Add ExpenseItem component here */
+                            // Expense item component
+                            <div className='col-sm'>
+                                <ExpenseItem/>
+                            </div>
                         }        
 
                         {
-                            /* Add AllocationForm component here under */
+                            // Allocation form component
+                            <div className='col-sm'>
+                                <AllocationForm/>
+                            </div>
                         }        
 
                 </div>
